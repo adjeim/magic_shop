40b0ca1b065b925a3ace62e824c83d2007ba9038
Mia Adjei <adjei.m@gmail.com>
2017-03-14 18:18:08 -0400

    Update user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+	validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+	
+	validates :password, presence: true, length: {minimum: 8, maximum: 32}, confirmation: true
+	validates :password_confirmation, presence: true
+
+	validates :fname, presence: true
+	validates :lname, presence: true
+
 	has_one :order
 	has_many :comments
 end

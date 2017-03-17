fd559fcd4cb81709b7ab9b35315cbf4aca0a9530
Mia Adjei <adjei.m@gmail.com>
2017-03-17 01:30:46 -0400

    Update users model and seeds.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+	devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 	validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }

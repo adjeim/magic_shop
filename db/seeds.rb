3bffc58ee57c8fd0665e4deeca4878644919c9f8
Mia Adjei <adjei.m@gmail.com>
2017-03-17 01:16:02 -0400

    Update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+	User.create(
+		fname: Faker::Name.first_name,
+		lname: Faker::Name.last_name,
+		username: Faker::Superhero.descriptor,
+		email: Faker::Internet.email,
+		password: "password"
+	)
+end
+
+(1..10).each do |num|
+	Order.create(
+		user_id: num
+	)
+end
+
+
+5.times do
+
+	Product.create(
+		name: "#{Faker::Superhero.unique.power} Potion",
+		price: Faker::Number.decimal(2, 2),
+		description: "A #{Faker::Color.color_name} potion. Use with caution."
+
+	)
+
+	Product.create(
+		name: "Enchanted #{Faker::Food.unique.ingredient}",
+		price: Faker::Number.decimal(2, 2),
+		description: "A magical ingredient harvested sustainably from the planet #{Faker::Space.planet}."
+
+	)
+
+end
